acd2c4300e1f796784a56e5f56e9b5a8e873ba42
devkaushik2003 <noorstagenstyle@gmail.com>
2024-10-02 19:49:13 +0530

    Update gal.tsx

--- pages/gal.tsx
+++ pages/gal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, Heading, Grid, Image } from '@chakra-ui/core';
+import { Flex, Heading, Grid, Image, Box } from '@chakra-ui/core';
 
 const GalleryPage: React.FC = () => {
   return (
@@ -18,101 +18,132 @@ const GalleryPage: React.FC = () => {
         px={['8', '8', '8', '24', '24']}  // Responsive padding on the sides
       >
         {/* Image 1 */}
-        <Image
-          src="/gallery/2 (1).jpg"
-          width="500px"    // Set fixed width
-          height="500px"   // Set fixed height
-          objectFit="cover"  // Ensure image fits box without distortion
-          loading='eager'
-        />
+        <Box
+          padding="10px"              // Padding inside the box for the frame effect
+          backgroundColor="#483C32"   // Taupe background for the frame
+          borderRadius="md"           // Optional: rounded corners for the frame
+        >
+          <Image
+            src="/gallery/2 (1).jpg"
+            width="500px"    // Set fixed width
+            height="500px"   // Set fixed height
+            objectFit="cover"  // Ensure image fits box without distortion
+            loading='eager'
+          />
+        </Box>
+        
         {/* Image 2 */}
-        <Image
-          src="/gallery/2 (2).JPG"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (2).JPG"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
+
         {/* Image 3 */}
-        <Image
-          src="/gallery/2 (3).jpg"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
-        {/* Image 4 */}
-        <Image
-          src="/gallery/2 (4).JPG"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
-        {/* Image 5 */}
-        <Image
-          src="/gallery/2 (5).JPG"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
-        {/* Image 6 */}
-        <Image
-          src="/gallery/2 (6).JPG"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
-        {/* Image 7 */}
-        <Image
-          src="/gallery/2 (7).JPG"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
-        {/* Image 8 */}
-        <Image
-          src="/gallery/2 (8).jpg"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
-        {/* Image 9 */}
-        <Image
-          src="/gallery/2 (9).jpg"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
-        {/* Image 10 */}
-        <Image
-          src="/gallery/2 (10).jpg"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
-        {/* Image 11 */}
-        <Image
-          src="/gallery/2 (11).jpg"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
-        {/* Image 12 */}
-        <Image
-          src="/gallery/2 (12).jpg"
-          width="500px"
-          height="500px"
-          objectFit="cover"
-          loading='eager'
-        />
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (3).jpg"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
+
+        {/* Repeat for other images */}
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (4).JPG"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
+
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (5).JPG"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
+
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (6).JPG"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
+
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (7).JPG"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
+
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (8).jpg"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
+
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (9).jpg"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
+
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (10).jpg"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
+
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (11).jpg"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
+
+        <Box padding="10px" backgroundColor="#483C32" borderRadius="md">
+          <Image
+            src="/gallery/2 (12).jpg"
+            width="500px"
+            height="500px"
+            objectFit="cover"
+            loading='eager'
+          />
+        </Box>
       </Grid>
     </Flex>
   );
